4a15c6a1ec1d05d167a51f95488dd5badd2e0de7
Polyisoprene <lzxder@outlook.com>
2025-03-11 17:55:27 +0800

    完善了数据库的创建

--- ariel/plugins/Core/ariel_database.py
+++ ariel/plugins/Core/ariel_database.py
@@ -4,29 +4,91 @@ from typing import Optional
 
 class DataManager:
     def __init__(self):
-        self.conn: Optional[aiosqlite.Connection] = None
-        self.cursor:Optional[aiosqlite.Cursor] = None
+        self._conn: Optional[aiosqlite.Connection] = None
+        self._cursor:Optional[aiosqlite.Cursor] = None
+        self.dbexists = True
 
     async def __aenter__(self):
         db_path = path.join(getcwd(),"data.sqlit")
-        self.conn = await aiosqlite.connect(db_path)
-        self.cursor = await self.conn.cursor()
+        if not path.exists(db_path):
+            self.dbexists=False
+        self._conn = await aiosqlite.connect(db_path)
+        self._cursor = await self.conn.cursor()
         await self._cursor.execute("BEGIN")
+        if not self.dbexists:
+            await self._cursor.execute("PRAGMA foreign_keys = ON;")
+            await self._creat_subTarget_table()
+            await self._creat_subChennal_table()
+            await self._creat_botStatus_table()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if exc_type is None:
-            await self.conn.commit()
+            await self._conn.commit()
         else:
-            await self.conn.rollback()
+            await self._conn.rollback()
         await self._cursor.close()
-        await self.conn.close()
+        await self._conn.close()
         self._cursor = None
-        self.conn = None
+        self._conn = None
         return False
     
-
-    async def creat_user_table(self):
-        pass
+    async def _creat_subTarget_table(self):
+        sql ="""
+            CREATE TABLE IF NOT EXISTS subTarget (
+                index INTEGER PRIMARY KEY AUTOINCREMENT, 
+                nickname TEXT NOT NULL,
+                uid TEXT NOT NULL,
+                live_status INTEGER NOT NULL DEFAULT 1 CHECK(live_active IN (0, 1)),
+            );
+            """
         
+        await self.cursor.execute(sql)
+    
+    async def _creat_subChennal_table(self):
+        sql ="""
+            CREATE TABLE IF NOT EXISTS subChennal (
+                index INTEGER PRIMARY KEY AUTOINCREMENT, 
+                uid TEXT NOT NULL,
+                group INTEGER NOT NULL,
+                bot INTEGER NOT NULL,
+                live_active INTEGER NOT NULL DEFAULT 1 CHECK(live_active IN (0, 1)),
+                dyn_active INTEGER NOT NULL DEFAULT 1 CHECK(live_active IN (0, 1)),
+                FOREIGN KEY (uid) 
+                REFERENCES subTarget(uid) 
+                ON DELETE CASCADE
+            );
+            """
+        await self.cursor.execute(sql)
+        
+    async def _creat_botStatus_table(self):
+        sql ="""
+            CREATE TABLE IF NOT EXISTS subChennal (
+                index INTEGER PRIMARY KEY AUTOINCREMENT, 
+                bot INTEGER NOT NULL,
+                group INTEGER NOT NULL,
+                push_active INTEGER NOT NULL DEFAULT 1 CHECK(live_active IN (0, 1)),
+                bot_active INTEGER NOT NULL DEFAULT 1 CHECK(live_active IN (0, 1)),
+                FOREIGN KEY (uid) 
+                REFERENCES subTarget(uid) 
+                ON DELETE CASCADE
+            );
+            """
+        await self._cursor.execute(sql)
+            
+    async def _creat_cookie_table(self):
+        sql ="""
+            CREATE TABLE IF NOT EXISTS subChennal (
+                index INTEGER PRIMARY KEY AUTOINCREMENT, 
+                bot INTEGER NOT NULL,
+                group INTEGER NOT NULL,
+                push_active INTEGER NOT NULL DEFAULT 1 CHECK(live_active IN (0, 1)),
+                bot_active INTEGER NOT NULL DEFAULT 1 CHECK(live_active IN (0, 1)),
+                FOREIGN KEY (uid) 
+                REFERENCES subTarget(uid) 
+                ON DELETE CASCADE
+            );
+            """
+        await self._cursor.execute(sql)
+
     
